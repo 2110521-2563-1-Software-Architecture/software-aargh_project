48baca60992c244a8f18bae7bdd8a4927a489355
focusjk <jiraphat.khupanit@gmail.com>
2020-11-16 12:53:10 +0700

    add chat detail

--- routes/chat.js
+++ routes/chat.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const ChatService = require('../services/chat');
+const UserService = require('../services/user');
 const passport = require('passport')
 
 const admin = require('firebase-admin');
@@ -28,26 +29,28 @@ router.post('/create',
     }
   });
 
-// router.post('/detail',
-//   passport.authenticate('jwt', { session: false }),
-//   async (req, res) => {
-//     const { _id: user_id } = req.user
-//     const { id } = req.body;
-//     const result = await ChatService.find({ _id: id })
-//     try {
-//       if (result.length === 0) {
-//         res.status('404').send({ message: 'Not found chat id!' });
-//       } else {
-//         const { messages, uid } = result[0]
-//         if (uid.indexOf(user_id) >= 0) {
-//           res.send({ messages, uid });
-//         } else {
-//           res.status(400).send({ message: 'This user is not in the chat!!' });
-//         }
-//       }
-//     } catch {
-//       res.status(400).send({ message: 'Unknown Error' });
-//     }
-//   });
+router.post('/detail',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    const { _id: user_id } = req.user
+    const { id } = req.body;
+    const result = await ChatService.find({ _id: id })
+    try {
+      if (result.length === 0) {
+        res.status('404').send({ message: 'Not found chat id!' });
+      } else {
+        const { messages, uid } = result[0]
+        if (uid.indexOf(user_id) >= 0) {
+          const query = uid.map(i => ({ _id: i }))
+          const users = await UserService.find({ '$or': query })
+          res.send({ messages, uid: users.map(i => ({ id: i._id, username: i.username })) });
+        } else {
+          res.status(400).send({ message: 'This user is not in the chat!!' });
+        }
+      }
+    } catch {
+      res.status(400).send({ message: 'Unknown Error' });
+    }
+  });
 
 module.exports = router;
